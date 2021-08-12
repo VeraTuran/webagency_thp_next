f4e9afc87140b2a5d74484e04317aca7717f3a85
Celia Lobet <lobet.c@gmail.com>
2021-08-12 21:24:06 +0200

    dark mode partial

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import HomePage from "./pages/HomePage";
 import About from "./pages/About";
 import Works from "./pages/Works";
 import Navigation from "./components/Navigation";
-import ThemeContextProvider from "Context/ThemeContext";
 import ToggleBtn from "components/ThemeToggleButton";
 import { ThemeContext } from "Context/ThemeContext";
 
@@ -13,25 +12,23 @@ const App = () => {
   return (
     <div className={theme ? "globaltheme light" : "globaltheme dark"}>
       <Router>
-        
-          <Navigation />
-          <ToggleBtn />
-          <main>
-            <Switch>
-              <div>
-                <Route path="/" exact>
-                  <HomePage />
-                </Route>
-                <Route path="/about" exact>
-                  <About />
-                </Route>
-                <Route path="/works" exact>
-                  <Works />
-                </Route>
-              </div>
-            </Switch>
-          </main>
-
+        <Navigation />
+        <ToggleBtn />
+        <main>
+          <Switch>
+            <div>
+              <Route path="/" exact>
+                <HomePage />
+              </Route>
+              <Route path="/about" exact>
+                <About />
+              </Route>
+              <Route path="/works" exact>
+                <Works />
+              </Route>
+            </div>
+          </Switch>
+        </main>
       </Router>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./components/ThemeToggleButton/ToggleButton.css";
+import "./style/index.scss";
 import App from "./App";
 import ThemeContextProvider from "Context/ThemeContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeContextProvider>
-      <App/>
+      <App />
     </ThemeContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
