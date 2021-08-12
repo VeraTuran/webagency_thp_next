13f3e5855744a8d94e8b26bc62dac33077369064
Celia Lobet <lobet.c@gmail.com>
2021-08-12 21:40:02 +0200

    dark mode ok

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navigation = () => {
   return (
     <div className="navigation">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
           <div className="navbar-brand">Websitic</div>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
